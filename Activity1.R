0f4217537d6a50d237b6052ea8f93d6204cbcb64
kmairiwright <kmwright@colgate.edu>
2025-09-05 11:12:48 -0400

    Question 2: vectors

--- Activity1.R
+++ Activity1.R
@@ -2,4 +2,11 @@ print("Hello World")
 a<-6234
 b<-c(2395,82,2947)
 b/2
-
+a*5
+a/3
+variable1<-c(5,21,300,21)
+variable1*5
+variable2<-c(17,2003,34,1)
+variable2*variable1
+variable3<-c(29,98,1002,3)
+variable3^variable2
