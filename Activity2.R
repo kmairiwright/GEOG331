c0a0164dd93fbbfbe533f247a711d07ea9f97915
kmairiwright <kmwright@colgate.edu>
2025-09-11 11:12:19 -0400

    Vectors and Matrices, 9/11

---
+++ Activity2.R
@@ -0,0 +1,33 @@
+##New script for F25 HW2##
+
+#make a vector of tree heights in meters
+heights <- c(30,41,20,22)
+#convert to cm
+heights_cm <- heights*100
+heights_cm
+#look at the first tree height
+heights[1]
+#look at the 2nd and 3rd tree heights
+heights[2:3]
+
+help("matrix")
+?matrix
+#testing if heights is a matrix
+is.matrix(heights)
+
+#set up a matrix with 2 columns and fill in by rows
+#first argument is the vector of numbers to fill in the matrix
+Mat<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=TRUE)
+Mat
+
+#set up a matrix that fills in by columns
+#first argument is the vector of numbers to fill in the matrix
+Mat.bycol<-matrix(c(1,2,3,4,5,6), ncol=2, byrow=FALSE)
+Mat.bycol
+#subset the matrix to look at row 1, column2
+Mat.bycol[1,2]
+#look at all values in row 1
+Mat.bycol[1,]
+#look at all values in column 2
+Mat.bycol[,2]
+
