737c2c973fb87cb2a461639a8b00d9ca8a4e1c93
kmairiwright <kmwright@colgate.edu>
2025-09-25 09:50:07 -0400

    Begin. Activity 3, Testing code and creating assert function.

---
+++ Activity3.R
@@ -0,0 +1,12 @@
+#create a function. The names of the arguments for your function will be in parentheses. Everything in curly brackets will be run each time the function is run.
+assert <- function(statement,err.message){
+  #if evaluates if a statement is true or false for a single item
+  if(statement == FALSE){
+    print(err.message)
+  }
+  
+}
+
+#check how the statement works
+#evaluate a false statement
+assert(1 == 2, "error: unequal values")
