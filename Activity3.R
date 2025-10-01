3529788f24300ad7de93c1b6d679dda9935f8bad
kmairiwright <kmwright@colgate.edu>
2025-10-01 16:51:18 -0400

    Answered through question 6, lubridate as comment now

--- Activity3.R
+++ Activity3.R
@@ -42,6 +42,103 @@ print(datW[1,])
 help("read.csv")
 
 #use install.packages to install lubridate
-install.packages(c("lubridate"))
+#install.packages(c("lubridate"))
 #it is helpful to comment this line after you run this line of code on the computer
 #and the package installs. You really don't want to do this over and over again.
+#working on new computer don't forget uncomment and run lubridate
+
+library(lubridate)
+
+#convert to standardized format
+#date format is m/d/y
+dates <- mdy_hm(datW$timestamp, tz= "America/New_York")
+
+#calculate day of year
+datW$doy <- yday(dates)
+#calculate hour in the day
+datW$hour <- hour(dates) + (minute(dates)/60)
+#calculate decimal day of year
+datW$DD <- datW$doy + (datW$hour/24)
+#quick preview of new date calculations
+datW[1,]
+
+#see how many values have missing data for each sensor observation
+#air temperature
+length(which(is.na(datW$air.temperature)))
+#wind speed
+length(which(is.na(datW$wind.speed)))
+#precipitation
+length(which(is.na(datW$precipitation)))
+#soil temperature
+length(which(is.na(datW$soil.moisture)))
+#soil moisture
+length(which(is.na(datW$soil.temp)))
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$soil.moisture, pch=19, type="b", xlab = "Day of Year",
+     ylab="Soil moisture (cm3 water per cm3 soil)")
+
+#make a plot with filled in points (using pch)
+#line lines
+plot(datW$DD, datW$air.temperature, pch=19, type="b", xlab = "Day of Year",
+     ylab="Air temperature (degrees C)")
+
+#I'm going to make a new column to work with that indicates that I am conducting QAQC
+#because overwriting values should be done cautiously and can lead to confusing issues.
+#It can be particularly confusing when you are just learning R.
+#Here I'm using the ifelse function
+#the first argument is a logical statement to be evaluated as true or false on a vector
+#the second argument is the value that my air.tempQ1 column will be given if the statement
+#is true. The last value is the value that will be given to air.tempQ1 if the statement is false.
+#In this case it is just given the air temperature value
+datW$air.tempQ1 <- ifelse(datW$air.temperature < 0, NA, datW$air.temperature)
+
+#check the values at the extreme range of the data
+#and throughout the percentiles
+quantile(datW$air.tempQ1)
+
+#look at days with really low air temperature
+datW[datW$air.tempQ1 < 8,]  
+#look at days with really high air temperature
+datW[datW$air.tempQ1 > 33,]  
+
+#plot precipitation and lightning strikes on the same plot
+#normalize lighting strikes to match precipitation
+lightscale <- (max(datW$precipitation)/max(datW$lightning.acvitivy)) * datW$lightning.acvitivy
+#make the plot with precipitation and lightning activity marked
+#make it empty to start and add in features
+plot(datW$DD , datW$precipitation, xlab = "Day of Year", ylab = "Precipitation & lightning",
+     type="n")
+#plot precipitation points only when there is precipitation 
+#make the points semi-transparent
+points(datW$DD[datW$precipitation > 0], datW$precipitation[datW$precipitation > 0],
+       col= rgb(95/255,158/255,160/255,.5), pch=15)        
+
+#plot lightning points only when there is lightning     
+points(datW$DD[lightscale > 0], lightscale[lightscale > 0],
+       col= "tomato3", pch=19)
+
+### QUESTION 5 ###
+
+assert(length(lightscale) == nrow(datW), "error: unequal rows")
+
+#filter out storms in wind and air temperature measurements
+# filter all values with lightning that coincides with rainfall greater than 2mm or only rainfall over 5 mm.    
+#create a new air temp column
+datW$air.tempQ2 <- ifelse(datW$precipitation  >= 2 & datW$lightning.acvitivy >0, NA,
+                          ifelse(datW$precipitation > 5, NA, datW$air.tempQ1))
+
+### Question 6 ###
+
+#create a new wind speed column with filtered data
+datW$wind.speedQ1 <- ifelse(datW$precipitation >= 2 & datW$lightning.acvitivy >0, NA, 
+                      ifelse(datW$precipitation > 5, NA, datW$wind.speed))
+
+#sum(is.na()) will return number of NAs in the data 
+assert(sum(is.na(datW$wind.speed))==sum(is.na(datW$wind.speedQ1)), "error: unequal NAs")
+
+#plotting wind speed (q1) data, type = b for lines and points
+plot(datW$DD, datW$wind.speedQ1, pch = 20, lwd = 1.5, type = "b", xlab="Day of the Year", ylab="Wind Speed (m/s)")
+
+
