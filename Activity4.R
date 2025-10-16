edd9076d8a24d2714afc198c77ef8671845247ef
kmairiwright <kmwright@colgate.edu>
2025-10-16 11:14:41 -0400

    thinking through forloop -- keep thinking

--- Activity4.R
+++ Activity4.R
@@ -12,3 +12,20 @@ library(tidyverse)
 ## Part 1: for loops##
 #using only data for iris versicolor
 flower<-iris[iris$Species=="versicolor",]
+
+#write a for loop
+#that produces a regression table
+#for each of the following relationships
+#1. iris  sepal length x width
+#2. iris  petal length x width
+#3. iris sepal length x petal length
+
+# hint: consider using a list, and also new vectors for regression variables
+
+regtab_list<-list()
+
+regvar<-c("flower$Sepal.Length", "flower$Sepal.Width", "flower$Petal.Length", "flower$Petal.Width")
+
+for(regvar in 1:4){
+  lm(regvar)
+}
