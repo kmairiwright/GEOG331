f29acef92d5a2387e189f8751ffbbf5e2a233e74
kmairiwright <kmwright@colgate.edu>
2025-10-17 16:07:06 -0400

    Finished part 2, now working through creating a ggplot

--- Activity4.R
+++ Activity4.R
@@ -9,7 +9,9 @@ head(iris)
 #install.packages(c("tidyverse"))
 library(tidyverse)
 
-## Part 1: for loops##
+#######################
+## Part 1: for loops ##
+#######################
 
 #iris versicolor data
 flower<-iris[iris$Species=="versicolor",]
@@ -26,3 +28,38 @@ regresults<-list()
 for(i in 1:3){
   regresults [[i]]<-lm(as.formula(regforms1[i]), flower)
 }
+
+###########################
+## Part 2: data in dplyr ##
+###########################
+
+#use dplyr to join data of max. height
+#to new iris data frame
+
+height<-data.frame(Species=c("virginica","setosa","versicolor"),
+                   Height.cm=c(60,100,11.8))
+#load in dplyr
+library(dplyr)
+
+Iris<-iris
+
+#create new Iris data frame including height
+IrisHeight<-left_join(Iris,height,by="Species")
+
+##############################
+## Part 3: plots in ggplot2 ##
+##############################
+
+#look at base R scatter plot
+plot(iris$Sepal.Length,iris$Sepal.Width)
+
+#3a. make the same plot in ggplot
+#load in ggplot2
+library(ggplot2)
+
+ggplot(data=iris, 
+       aes=(x=iris$Sepal.Length, y=iris$Sepal.Width)
+       + geom_point())
+
+        
+
