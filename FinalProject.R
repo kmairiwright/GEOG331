de1caee890103d7e4ee72dc5484031ef32be4d2d
kmairiwright <kmwright@colgate.edu>
2025-11-06 11:37:59 -0500

    Trying to fix nlcd error

--- FinalProject.R
+++ FinalProject.R
@@ -5,18 +5,16 @@ rm(list=ls())
 #install.packages(c("ggplot2"))
 #install.packages(c("tidyverse"))                 
 #install.packages(c("FedData"))
-#install.packages(c("terra"))
-#install.packages(c("tidyterra"))
 #install.packages(c("tigris"))
-library(terra)
-library(tidyterra)
+#install.packages(c("sf"))
 library(FedData)
 library(ggplot2)
 library(tigris)
+library(sf)
 
 #NOAA data from 1/1/2001 to 12/31/2021 of flash floods
 #in Grand County, Utah
-NOAAFlashFlood<-read.csv("Z:\\kmwright\\data\\storm_data_search_results.csv")
+NOAAFlashFlood<-read.csv("Z:\\kmwright\\data\\Project_Data\\storm_data_search_results.csv")
 
 #create histogram of flash flood reports, monthly
 NOAAFlashFlood$BEGIN_DATE<-as.Date(NOAAFlashFlood$BEGIN_DATE,format="%m/%d/%Y")
@@ -30,12 +28,14 @@ ggplot(data=NOAAFlashFlood,
 
 #Land cover dataset using FedData
 Grand_County_UT<-vect("Z:\\kmwright\\data\\Project_Data\\Grand_County")
-terra::plot(Grand_County_UT)
+
+Grand_County_sf<-sf::st_as_sf(Grand_County_UT)
 
 Grand_County_Tigris<-counties(state="UT",cb=TRUE) %>%
   filter(NAME=="Grand")
 
-Grand_2015<-get_nlcd(template=Grand_County_Tigris,
+Grand_2015<-download_nlcd(template=Grand_County_Tigris,
                      label="Grand County UT",
                      year=2015,
-                     extraction.dir="Z:\\kmwright\\data")
+                     extraction.dir="Z:\\kmwright\\data\\Project_Data")
+
